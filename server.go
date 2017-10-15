22cb6ce9356fe589c7e5afd3c3cf38fa84e83417
mdabagia <maxdabagia@gmail.com>
2017-10-14 21:43:13 -0400

    Added encryption

--- server.go
+++ server.go
@@ -3,9 +3,9 @@ package main
 import (
     "encoding/json"
     "fmt"
-    //"io"
     "net/http"
-    //"log"
+    "log"
+    "os"
     "github.com/rs/cors"
     "github.com/turnage/graw/reddit"
 )
@@ -20,6 +20,80 @@ type recievedImage struct {
     Image string
 }
 
+func GetKey(fileName string) []uint8 {
+    f, err := os.Open(fileName)
+    if err != nil {
+        log.Fatal(err)
+    }
+    b1 := make([]byte, 1)
+    key := make([]byte, 10000)
+    for i := range key {
+        _, err = f.Read(b1)
+        if err != nil {
+            log.Fatal(err)
+        }
+        if b1[0] - 48 <= 9 {
+            key[i] = b1[0] - 48
+        } else {
+            for b1[0] - 48 > 9 {
+                _, err = f.Read(b1)
+                if err != nil {
+                    log.Fatal(err)
+                }
+            }
+            key[i] = b1[0] - 48
+        }
+    }
+    //fmt.Println(key)
+    return key
+    
+    
+}
+
+func Encode(arr []uint8, key []uint8) []uint8 {
+    for i := range arr {
+        if arr[i] >= 97 && arr[i] <= 122 {
+            arr[i] += key[i]   
+            if arr[i] > 122 {
+                arr[i] -= 26
+            }
+        } else if arr[i] >= 65 && arr[i] <= 90 {
+            arr[i] += key[i]   
+            if arr[i] > 90 {
+                arr[i] -= 26
+            }
+        } else if arr[i] >= 48 && arr[i] <= 57 {
+            arr[i] += key[i]
+            if arr[i] > 57 {
+                arr[i] -= 10
+            }
+        }
+        
+    }
+    return arr
+}
+
+func Decode(arr []uint8, key []uint8) []uint8 {
+    for i := range arr {
+        if arr[i] >= 97 && arr[i] <= 122 {
+            if arr[i] <= 96 + key[i] {
+                arr[i] += 26
+            }
+            arr[i] -= key[i]
+        } else if arr[i] >= 65 && arr[i] <= 90 {
+            if arr[i] <= 64 + key[i]{
+                arr[i] += 26
+            }
+            arr[i] -= key[i]
+        } else if arr[i] >= 48 && arr[i] <= 57 {
+            if arr[i] <= 47 + key[i] {
+                arr[i] += 10
+            }
+            arr[i] -= key[i]
+        }
+    }
+    return arr
+}
 
 func GetText(w http.ResponseWriter, req *http.Request) {
     decoder := json.NewDecoder(req.Body)
@@ -29,8 +103,9 @@ func GetText(w http.ResponseWriter, req *http.Request) {
 	if err != nil {
 		panic(err)
 	}
-
-
+    
+    t.Key = string(Encode([]uint8(t.Key), GetKey("E.txt") ))
+    t.Post = string(Encode([]uint8(t.Post), GetKey("E.txt") ))
     err = bot.PostSelf("/r/SecretHandshakeVault", t.Key, t.Post)
     if err != nil {
         fmt.Println("Failed to fetch /r/golang: ", err)
@@ -49,7 +124,9 @@ func GetImage(w http.ResponseWriter, req *http.Request) {
 	if err != nil {
 		panic(err)
 	}
-
+    
+ //   i.Key = string(Encode([]uint8(i.Key), GetKey("E.txt") ))
+   // i.Image = string(Encode([]uint8(i.Image), GetKey("E.txt") ))
     err = bot.PostSelf("/r/SecretHandshakeVault", i.Key, i.Image[0:10])
     if err != nil {
         fmt.Println("Failed to post image ", err)
@@ -81,7 +158,6 @@ func main() {
 
     handler := c.Handler(mux)
     http.ListenAndServe(":3000", handler)
-
-
+    
 
 }
